11fc966313d8e091f678f46a9a548c6763cff195
Imanoi de Oz <iozu.dono@gmail.com>
2023-03-06 23:50:32 +0100

    segundodia

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -10,8 +10,8 @@ export class SimpleActorSheet extends ActorSheet {
   /** @inheritdoc */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["worldbuilding", "sheet", "actor"],
-      template: "systems/worldbuilding/templates/actor-sheet.html",
+      classes: ["ren", "sheet", "actor"],
+      template: "systems/ren/templates/actor-sheet.html",
       width: 600,
       height: 600,
       tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
@@ -25,8 +25,8 @@ export class SimpleActorSheet extends ActorSheet {
   /** @inheritdoc */
   async getData(options) {
     const context = await super.getData(options);
-    EntitySheetHelper.getAttributeData(context.data);
-    context.shorthand = !!game.settings.get("worldbuilding", "macroShorthand");
+    /** EntitySheetHelper.getAttributeData(context.data);*/
+    context.shorthand = !!game.settings.get("ren", "macroShorthand");
     context.systemData = context.data.system;
     context.dtypes = ATTRIBUTE_TYPES;
     context.biographyHTML = await TextEditor.enrichHTML(context.systemData.biography, {
